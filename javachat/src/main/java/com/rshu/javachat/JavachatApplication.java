4fa64896c912a8e7bc625e4cf4922a2cdebd5c66
Li595 <61666020+Li595@users.noreply.github.com>
2022-06-06 15:05:06 +0300

    скелет чата
    по идее должно работать если прикрутить хранение логинов и сообщений в бд
    ещё нет добавления файлов

---
+++ javachat/src/main/java/com/rshu/javachat/JavachatApplication.java
@@ -0,0 +1,14 @@
+package com.rshu.javachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavachatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavachatApplication.class, args);
+	}
+
+}
